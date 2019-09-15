a27df8fccb02cd618fd20df85e48dee37db17897
Edgar Sanchez <garosanchez27@gmail.com>
2019-09-15 01:06:53 -0500

    Created seed.rb file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blogpost #{blog}",
+    body: "Marcus Aurelius was Roman emperor from 161 to 180 and a Stoic philosopher. He was the last of the rulers known as the Five Good Emperors, and the last emperor of the Pax Romana, an age of relative peace and stability for the Roman Empire. He served as Roman consul in 140, 145, and 161."
+  )
+end
+
+puts "10 blogposts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Skill ##{skill}",
+    skillset_percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+10.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Porfolio Title #{portfolio_item}",
+    subtitle: "Some great stuff",
+    body: "You have power over your mind - not outside events. Realize this, and you will find strength.",
+    main_image: "https://place-hold.it/600x400",
+    thumb_image: "https://place-hold.it/150x100"
+  )
+end
+
+puts "9 portfolio items created"
