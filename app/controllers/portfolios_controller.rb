9ea4c25c340a3066b01ca673e24b81a08e491130
Edgar Sanchez <garosanchez27@gmail.com>
2019-09-15 01:20:20 -0500

    Added index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,24 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def show
+  end 
+  
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
