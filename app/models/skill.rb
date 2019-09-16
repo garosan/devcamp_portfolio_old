e4d4ea354e1651fc74a3f38f6616f20e58b48b15
Edgar Sanchez <garosanchez27@gmail.com>
2019-09-16 11:49:07 -0500

    Added custom scopes for portfolio items

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,3 @@
 class Skill < ApplicationRecord
-  validate_presence_of :title, :skillset_percent_utilized
+  validates_presence_of :title, :skillset_percent_utilized
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blogpost #{blog}",
-    body: "Marcus Aurelius was Roman emperor from 161 to 180 and a Stoic philosopher. He was the last of the rulers known as the Five Good Emperors, and the last emperor of the Pax Romana, an age of relative peace and stability for the Roman Empire. He served as Roman consul in 140, 145, and 161."
+    body: "Marcus Aurelius was Roman emperor from 161 to 180 and a Stoic philosopher. He was the last of the rulers known as the Five Good Emperors, and the last emperor of the Pax Romana, an age of relative peace and stability for the Roman Empire. He served as Roman consul in 140, 145, and 161.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created"
 
-10.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Porfolio Title #{portfolio_item}",
-    subtitle: "Some great stuff",
+    subtitle: "Ruby on Rails",
+    body: "You have power over your mind - not outside events. Realize this, and you will find strength.",
+    main_image: "https://place-hold.it/600x400",
+    thumb_image: "https://place-hold.it/150x100"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Porfolio Title #{portfolio_item}",
+    subtitle: "React",
     body: "You have power over your mind - not outside events. Realize this, and you will find strength.",
     main_image: "https://place-hold.it/600x400",
     thumb_image: "https://place-hold.it/150x100"
