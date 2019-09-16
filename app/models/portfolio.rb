af899926dd43f1165cb9db6630939aa5406f0851
Edgar Sanchez <garosanchez27@gmail.com>
2019-09-16 12:23:48 -0500

    Added concern for image urls

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   
   def self.react
@@ -10,7 +11,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://place-hold.it/600x400"
-    self.thumb_image ||= "https://place-hold.it/150x100"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '150', width: '100')
   end
 end
